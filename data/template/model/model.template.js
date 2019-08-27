cbe99719d5b703bc33c0f5e6742141d67a1fe388
Lcnvdl <programando.soft@gmail.com>
2019-08-27 00:10:16 -0300

    Avances

---
+++ data/template/model/model.template.js
@@ -0,0 +1,9 @@
+const { Schema, model } = require("mongoose");
+
+const __Name__Schema = new Schema({
+  //  __@Definitions__
+});
+
+const __Name__Model = model("__Name__", __Name__Schema);
+
+module.exports = __Name__Model;
