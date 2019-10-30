4583b4360d7e2fa071ad418ee2af43ede0010471
Lcnvdl <programando.soft@gmail.com>
2019-10-30 07:59:17 -0300

    Pendientes

---
+++ tester.js
@@ -0,0 +1,17 @@
+/*const MongoosePlugin = require("./src/mongoose.plugin");
+
+const { version, addModel } = require("./src/commands/commands");
+
+const colog = require("colog");
+const inquirer = require("inquirer");
+const commander = require("commander");
+
+const program = new commander.Command();
+
+let instance = new MongoosePlugin();
+
+const cmdObj = {};
+const classes = {};
+const methods = {};
+
+addModel("TestModel", cmdObj, colog, inquirer)*/
